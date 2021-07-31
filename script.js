df0fe96c02793f1162a1b9ca70614c6ae042f296
Yuri Araújo <71369804+sempiternus-bit@users.noreply.github.com>
2021-07-31 17:41:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ function load(){
 	let horas = data.getHours()
 	d1.innerHTML = `Agora são ${horas} horas`
 	if (12 >= horas && horas < 13){
-		img.src = 'im1.jpg'
+		img.src = 'img1.png'
 		document.body.style.background = '#e2cd0f'
 
 	} else if (13 <= horas && horas < 18){
-		img.src = 'im2.jpeg'
+		img.src = 'img2.png'
 		document.body.style.background = '#b9846f'
 	} else{
-		img.src = 'im3.jpg'
+		img.src = 'img3.png'
 		document.body.style.background = '#515154'
 	}
 }
